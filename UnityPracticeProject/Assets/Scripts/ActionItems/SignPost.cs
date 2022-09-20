4685f40ab54840ea0ae7b2e72a25321dab807ffb
Trey0303 <61602725+Trey0303@users.noreply.github.com>
2022-09-20 14:12:36 -0700

    followed a tutorial for making character stats. some of the stuff I have yet to understand.

--- UnityPracticeProject/Assets/Scripts/ActionItems/SignPost.cs
+++ UnityPracticeProject/Assets/Scripts/ActionItems/SignPost.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SignPost : ActionItem
 {
+    public string[] dialogue;
     public override void Interact()
     {
         //base.Interact(); would call the default Interact() inside the ActionItem script. then it would also call the Interact function inside this script after
 
+        DialogueSystem.Instance.AddNewDialogue(dialogue,"sign");
         Debug.Log("interacting with sign post");
     }
 }
---
+++ UnityPracticeProject/Assets/Scripts/BaseStat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStat
+{
+    public List<StatBonus> BaseAdditives { get; set; }
+    public int BaseValue { get; set; }//starting stat
+    public string StatName { get; set; }
+    public string StatDescription { get; set; }
+    public int FinalValue { get; set; }//a value that takes the starting stat and calculates it with buffs and defuffs accordingly
+
+    public BaseStat(int baseValue, string statName, string statDescription)
+    {
+        this.BaseAdditives = new List<StatBonus>();
+        this.BaseValue = baseValue;
+        this.StatName = statName;
+        this.StatDescription = statDescription;
+    }
+    
+    public void AddStatBonus(StatBonus statBonus)
+    {
+        this.BaseAdditives.Add(statBonus);
+    }
+
+    public void RemoveStatBonus(StatBonus statBonus)
+    {
+        this.BaseAdditives.Remove(statBonus);
+    }
+
+    public int GetCalculatedStatValue()
+    {
+        this.BaseAdditives.ForEach(x => this.FinalValue += x.BonusValue);//????
+
+        FinalValue += BaseValue;
+
+        return FinalValue;
+    }
+}
