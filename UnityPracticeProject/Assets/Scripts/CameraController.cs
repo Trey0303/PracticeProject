bb31a54182946ba2332000cfaf851a7637c45e66
Trey0303 <61602725+Trey0303@users.noreply.github.com>
2022-10-04 14:49:59 -0700

    simple camera controller was made

---
+++ UnityPracticeProject/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float playerCameraDistance { get; set; }
+    public Transform cameraTarget;
+
+    Camera playerCamera;
+    float zoomSpeed = 35;
+
+    private void Start()
+    {
+        playerCameraDistance = 4;
+        playerCamera = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetAxisRaw("Mouse ScrollWheel") != 0)
+        {
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            playerCamera.fieldOfView -= scroll * zoomSpeed;
+            playerCamera.fieldOfView = Mathf.Clamp(playerCamera.fieldOfView, 10, 80);
+        }
+
+        transform.position = new Vector3(cameraTarget.position.x, cameraTarget.position.y + playerCameraDistance, cameraTarget.position.z - playerCameraDistance);
+    }
+}
