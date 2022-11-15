955d286a2e156ca42d621cc422b62999cd9fdca6
Trey0303 <61602725+Trey0303@users.noreply.github.com>
2022-11-15 10:25:33 -0800

    working on adding quests for the player to complete.

---
+++ UnityPracticeProject/Assets/Scripts/Questing/Goal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//quest goal will have a description, a boolean for knowing if the quest is completed, 
+//and the ability to keep track of an amount of something and compare that to how much is need to complete the quest(quest ex: collect 5 bolts, current amount is 0)
+public class Goal
+{
+    public string Description { get; set; }
+    public bool Completed { get; set; }
+    public int CurrentAmount { get; set; }
+    public int RequiredAmount { get; set; }
+
+    public virtual void Init()
+    {
+        //defaults
+    }
+
+    public void Evaluate()
+    {
+        if(CurrentAmount >= RequiredAmount)
+        {
+            Complete();
+        }
+    }
+
+    public void Complete()
+    {
+        Completed = true;
+    }
+}
