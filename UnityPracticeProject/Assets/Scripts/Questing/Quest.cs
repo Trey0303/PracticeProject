daa4270ae898586f971f1f40591bceb4ddeadffc
Trey0303 <61602725+Trey0303@users.noreply.github.com>
2022-11-18 15:21:37 -0800

    working on adding quests for the player to complete. created a script that should let the game know when the player has completed all the goals required to finish the quest and should give the player their reward.

---
+++ UnityPracticeProject/Assets/Scripts/Questing/Quest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+//base class for quests. will define properties and have some methods that all the quests will share.
+public class Quest : MonoBehaviour
+{
+    public List<Goal> Goals { get; set; } = new List<Goal>();
+    public string QuestName { get; set; }
+    public string Description { get; set; }
+    public int ExperienceReward { get; set; }
+    public Item ItemReward { get; set; }
+    public bool Completed { get; set; }
+
+    public void CheckGoals()
+    {
+        Completed = Goals.All(g => g.Completed); ///this this a simpler way to write out everything thats commmeted out below
+
+        if (Completed)
+        {
+            GiveReward();
+        }
+
+        //if (Goals.All(g => g.Completed))//if all goals are completed it will return true. if not then false
+        //{
+        //    Complete();
+        //}
+    }
+
+    //void Complete()
+    //{
+    //    Completed = true;
+    //}
+
+    void GiveReward()
+    {
+        if(ItemReward != null)
+        {
+            InventoryController.Instance.GiveItem(ItemReward);
+        }
+    }
+}
