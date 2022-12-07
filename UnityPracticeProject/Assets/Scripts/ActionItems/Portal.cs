5e5398803fb4d9e0e85d879c43e08a559c5c6337
Trey0303 <61602725+Trey0303@users.noreply.github.com>
2022-12-07 15:02:24 -0800

    working on a simple teleportation system that the player can use to teleport to different parts of the scene.

---
+++ UnityPracticeProject/Assets/Scripts/ActionItems/Portal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : ActionItem
+{
+    public Vector3 TeleportLocation { get; set; }
+    [SerializeField]
+    private Portal[] linkedPortals;
+
+    private void Start()
+    {
+        TeleportLocation = new Vector3(transform.position.x + 2f,transform.position.y, transform.position.z);
+    }
+
+    public override void Interact()
+    {
+        
+    }
+}
---
+++ UnityPracticeProject/Assets/Scripts/PortalController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PortalController : MonoBehaviour
+{
+    private Button button;
+    private Portal[] portal;
+    private Player player;
+    private GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        panel = transform.Find("Panel_Portals").gameObject;
+    }
+
+    public void ActivatePortal(Portal[] portals)
+    {
+        panel.SetActive(true);
+        for (int i = 0; i < portals.Length; i++){
+            Button portalButton = Instantiate(button, panel.transform);
+            portalButton.GetComponentInChildren<Text>().text = portals[i].name;
+        }
+    }
+}
